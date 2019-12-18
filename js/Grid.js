22289ab9400c7ab6e7d1f908595cca37adef9add
Markus <mr.markus.coll@gmail.com>
2019-12-18 09:20:17 +0100

    fixed final boss health

--- js/Grid.js
+++ js/Grid.js
@@ -323,7 +323,7 @@
                 
                     checkForFinalBoss(positionY, positionX){
                         if (this.grid[positionY][positionX] === 'F'){
-                          this.$refs.hero.fightFinalBoss(70);
+                          this.$refs.hero.fightFinalBoss(40);
                           this.grid[positionY][positionX] === ' '
                           if ((shownMessage2 == true) && (this.shownMessage5 == false)){
                             this.$refs.hero.updateMessage('storyMessage5'); 
